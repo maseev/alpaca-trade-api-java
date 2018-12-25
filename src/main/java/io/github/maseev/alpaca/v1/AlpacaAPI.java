37057f88fb52b213dd0bbab6493db99e3bccf00c
Miron Aseev <aseev.miron@gmail.com>
2018-12-25 16:30:32 +0700

    Add a base API class

---
+++ src/main/java/io/github/maseev/alpaca/v1/AlpacaAPI.java
@@ -0,0 +1,47 @@
+package io.github.maseev.alpaca.v1;
+
+import io.github.maseev.alpaca.http.HttpClient;
+import io.github.maseev.alpaca.v1.account.AccountAPI;
+import org.asynchttpclient.AsyncHttpClientConfig;
+
+public class AlpacaAPI {
+
+  public enum Type {
+    TEST,
+    LIVE
+  }
+
+  private static final String APCA_API_BASE_URL_PAPER_TRADING="https://paper-api.alpaca.markets/v1";
+  private static final String APCA_API_BASE_URL_LIVE="https://api.alpaca.markets/v1";
+
+  private final AccountAPI accountAPI;
+
+  public AlpacaAPI(String baseUrl, String keyId, String secretKey, AsyncHttpClientConfig config) {
+    HttpClient httpClient = new HttpClient(baseUrl, keyId, secretKey, config);
+
+    accountAPI = new AccountAPI(httpClient);
+  }
+
+  public AlpacaAPI(String baseUrl, String keyId, String secretKey) {
+    this(baseUrl, keyId, secretKey, null);
+  }
+
+  public AlpacaAPI(Type type, String keyId, String secretKey) {
+    this(getBaseUrl(type), keyId, secretKey);
+  }
+
+  public AccountAPI account() {
+    return accountAPI;
+  }
+
+  private static String getBaseUrl(Type type) {
+    switch (type) {
+      case TEST:
+        return APCA_API_BASE_URL_PAPER_TRADING;
+      case LIVE:
+        return APCA_API_BASE_URL_LIVE;
+      default:
+        throw new IllegalArgumentException(String.format("Unknown type; type: %s", type));
+    }
+  }
+}
