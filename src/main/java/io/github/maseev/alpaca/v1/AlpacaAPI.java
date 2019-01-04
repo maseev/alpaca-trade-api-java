e0d7612c4dfa5bfa15c343b3c15535e20cad14ff
Miron Aseev <aseev.miron@gmail.com>
2019-01-04 12:05:19 +0700

    Add an Order API to the main API class

--- src/main/java/io/github/maseev/alpaca/v1/AlpacaAPI.java
+++ src/main/java/io/github/maseev/alpaca/v1/AlpacaAPI.java
@@ -2,6 +2,7 @@ package io.github.maseev.alpaca.v1;
 
 import io.github.maseev.alpaca.http.HttpClient;
 import io.github.maseev.alpaca.v1.account.AccountAPI;
+import io.github.maseev.alpaca.v1.order.OrderAPI;
 import org.asynchttpclient.AsyncHttpClientConfig;
 
 public class AlpacaAPI {
@@ -15,11 +16,13 @@ public class AlpacaAPI {
   private static final String APCA_API_BASE_URL_LIVE="https://api.alpaca.markets/v1";
 
   private final AccountAPI accountAPI;
+  private final OrderAPI orderAPI;
 
   public AlpacaAPI(String baseUrl, String keyId, String secretKey, AsyncHttpClientConfig config) {
     HttpClient httpClient = new HttpClient(baseUrl, keyId, secretKey, config);
 
     accountAPI = new AccountAPI(httpClient);
+    orderAPI = new OrderAPI(httpClient);
   }
 
   public AlpacaAPI(String baseUrl, String keyId, String secretKey) {
@@ -32,6 +35,10 @@ public class AlpacaAPI {
 
   public AccountAPI account() {
     return accountAPI;
+  }
+
+  public OrderAPI orders() {
+    return orderAPI;
   }
 
   private static String getBaseUrl(Type type) {
