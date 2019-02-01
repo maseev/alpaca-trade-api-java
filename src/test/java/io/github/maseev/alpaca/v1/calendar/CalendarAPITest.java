16b88f4ac085b2328aa52fd457f5e83215abc960
Miron Aseev <aseev.miron@gmail.com>
2019-02-01 09:13:23 +0700

    Add a test for the 'get the Calendar' Calendar API method

---
+++ src/test/java/io/github/maseev/alpaca/v1/calendar/CalendarAPITest.java
@@ -0,0 +1,75 @@
+package io.github.maseev.alpaca.v1.calendar;
+
+import static io.github.maseev.alpaca.http.json.util.JsonUtil.toJson;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+import com.google.common.net.MediaType;
+import io.github.maseev.alpaca.APITest;
+import io.github.maseev.alpaca.http.HttpClient;
+import io.github.maseev.alpaca.http.HttpCode;
+import io.github.maseev.alpaca.http.exception.APIException;
+import io.github.maseev.alpaca.v1.AlpacaAPI;
+import io.github.maseev.alpaca.v1.calendar.entity.Calendar;
+import io.github.maseev.alpaca.v1.calendar.entity.ImmutableCalendar;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+
+public class CalendarAPITest extends APITest {
+
+  @Test(expected = IllegalArgumentException.class)
+  public void specifyingEndDateBeforeStartDateMustThrowException() throws APIException {
+    String validKeyId = "valid key";
+    String validSecretKey = "valid secret";
+    AlpacaAPI api = new AlpacaAPI(getBaseURL(), validKeyId, validSecretKey);
+
+    LocalDate start = LocalDate.now();
+    LocalDate end = start.minusDays(1);
+
+    api.calendar().get(start, end).await();
+  }
+
+  @Test
+  public void gettingListOfCalendarsWithinCorrectTimelineMustReturnExpectedCalendars() throws Exception {
+    String validKeyId = "valid key";
+    String validSecretKey = "valid secret";
+    AlpacaAPI api = new AlpacaAPI(getBaseURL(), validKeyId, validSecretKey);
+
+    LocalDate start = LocalDate.now();
+    LocalDate end = start.plusDays(10);
+
+    ImmutableCalendar expectedCalendar =
+      ImmutableCalendar.builder()
+        .date(start.plusDays(5))
+        .open(LocalTime.now())
+        .close(LocalTime.now().plusHours(5))
+        .build();
+
+    List<ImmutableCalendar> expectedCalendars = Collections.singletonList(expectedCalendar);
+
+    mockServer()
+      .when(
+        request("/calendar")
+          .withMethod(HttpClient.HttpMethod.GET.toString())
+          .withHeader(APCA_API_KEY_ID, validKeyId)
+          .withHeader(APCA_API_SECRET_KEY, validSecretKey)
+          .withQueryStringParameter("start", start.toString())
+          .withQueryStringParameter("end", end.toString())
+      )
+      .respond(
+        response()
+          .withStatusCode(HttpCode.OK.getCode())
+          .withBody(toJson(expectedCalendars), MediaType.JSON_UTF_8)
+      );
+
+    List<Calendar> calendars = api.calendar().get(start, end).await();
+
+    assertThat(calendars, is(equalTo(expectedCalendars)));
+  }
+}
